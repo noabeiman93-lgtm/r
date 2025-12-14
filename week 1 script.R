b2a2239605ec8e87c6bcc7adc3d1be9946baa0e4
noabeiman93-lgtm <noa.beiman93@gmail.com>
2025-12-14 08:23:15 +0200

    Initial commit

---
+++ week 1 script.R
@@ -0,0 +1,11 @@
+# Intro to R
+# Week1
+# 311246169 Noa Beiman
+
+subject_number = c (1,2,3,4,5,6)
+gender = c ("M", "F", "M", "F", "M", "F") 
+age=c(15,18,20,34,40,17) 
+depression = c (0,1,1,1,0,0)
+
+df = data.frame (subject_number, gender, age, depression) 
+write_csv (df, file = df.csv)
