8470cb9d446fa8c374b90a656361ba569cb1f6cb
noabeiman93-lgtm <noa.beiman93@gmail.com>
2025-12-14 08:24:13 +0200

    added anxiety variable

--- week 1 script.R
+++ week 1 script.R
@@ -6,6 +6,7 @@ subject_number = c (1,2,3,4,5,6)
 gender = c ("M", "F", "M", "F", "M", "F") 
 age=c(15,18,20,34,40,17) 
 depression = c (0,1,1,1,0,0)
+anxiety = c (0,1,1,1,1,0)
 
-df = data.frame (subject_number, gender, age, depression) 
+df = data.frame (subject_number, gender, age, depression, anxiety) 
 write_csv (df, file = df.csv)
